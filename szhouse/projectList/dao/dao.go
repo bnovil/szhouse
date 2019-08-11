beb7baf6cfdbcc7662e191cce26747579d3cb099
bnovil <lzqcn2000@126.com>
2019-08-11 10:13:23 +0800

    refactor dao

--- szhouse/projectList/dao/dao.go
+++ szhouse/projectList/dao/dao.go
@@ -3,28 +3,28 @@ package dao
 import (
 	"crawl/szhouse/projectList/data"
 	"log"
+	"sync"
 
-	_ "github.com/go-sql-driver/mysql"   //mysql driver
+	_ "github.com/go-sql-driver/mysql" //mysql driver
 	"github.com/jinzhu/gorm"
 )
 
 var db *gorm.DB
 
-func init() {
-	var err error
-	db, err = gorm.Open("mysql", dsn)
+// func init() {
+// var err error
+// db, err = gorm.Open("mysql", dsn)
 
-	if err != nil {
-		log.Println(err)
-	}
+// if err != nil {
+// 	log.Println(err)
+// }
 
-	// SetMaxIdleConns sets the maximum number of connections in the idle connection pool.
-	db.DB().SetMaxIdleConns(10)
+// // SetMaxIdleConns sets the maximum number of connections in the idle connection pool.
+// db.DB().SetMaxIdleConns(10)
 
-	// SetMaxOpenConns sets the maximum number of open connections to the database.
-	db.DB().SetMaxOpenConns(100)
-
-}
+// // SetMaxOpenConns sets the maximum number of open connections to the database.
+// db.DB().SetMaxOpenConns(100)
+// }
 
 const dsn string = "test:test123@tcp(localhost:3306)/szhouse?charset=utf8&parseTime=True&loc=Local"
 
@@ -33,7 +33,7 @@ Insert new record
 */
 func Insert(b data.ProjectBrief) bool {
 
-	e := db.Table("project_brief").Create(&b).Error
+	e := GetDBConn().Table("project_brief").Create(&b).Error
 	if e != nil {
 		log.Println(e)
 		return false
@@ -48,6 +48,30 @@ func Insert(b data.ProjectBrief) bool {
 
 }
 
-func openDb() {
+//GetDBConn get database connection
+func GetDBConn() *gorm.DB {
+
+	if db == nil {
+		var mu sync.Mutex
+		mu.Lock()
+		defer mu.Unlock()
+
+		log.Print("start connect to database")
+		var err error
+		db, err = gorm.Open("mysql", dsn)
+
+		if err != nil {
+			log.Println(err)
+		}
+
+		// SetMaxIdleConns sets the maximum number of connections in the idle connection pool.
+		db.DB().SetMaxIdleConns(10)
+
+		// SetMaxOpenConns sets the maximum number of open connections to the database.
+		db.DB().SetMaxOpenConns(10)
+
+		return db
+	}
+	return db
 
 }
--- szhouse/projectList/dao/dao_test.go
+++ szhouse/projectList/dao/dao_test.go
@@ -7,15 +7,13 @@ import (
 )
 
 func TestInsert(t *testing.T){
-	s := `{"Id":41053,"Seq":"1","PreNumber":"恒大时尚慧谷大厦","NumberLink":"/certdetail.aspx?id=41053","Name":"恒大时尚慧谷大厦","NameLink":"projectdetail.aspx?id=41053","Company":"建滔数码发展（深圳）有限公司","District":"宝安","ApproveTime":"2019-07-30"}`
+	s := `{"Id":10001,"Seq":"1","PreNumber":"恒大时尚慧谷大厦","NumberLink":"/certdetail.aspx?id=41053","Name":"恒大时尚慧谷大厦","NameLink":"projectdetail.aspx?id=41053","Company":"建滔数码发展（深圳）有限公司","District":"宝安","ApproveTime":"2019-07-30"}`
 	var b data.ProjectBrief
 
 	json.Unmarshal([]byte(s),&b)
 
 	f := Insert(b)
-	if f{
-		t.Log("insert succeed")
-	}else{
-		t.Log("insert failed")
+	if !f {
+		t.Error("insertt failed")
 	}
 }
